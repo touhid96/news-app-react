fa374e971b9737aac60afbe5caf6673740b9587f
Touhidul_Islam <touhid.islam96@gmail.com>
2022-04-01 23:43:20 +0600

    ready to deploy

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
+import Body from './components/Body/Body';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Body/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Body/Body.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import "../../App.css";
+import LoadingSpinner from "./LoadingSpinner";
+import News from "./News/News";
+
+const Body = () => {
+  const [article, setArticle] = useState(null);
+
+  useEffect(() => {
+    fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=346c7bdd368b4b0fb5724669204c3d81")
+      .then((res) => res.json())
+      .then((data) => setArticle(data.articles));
+  }, []);
+
+  return (
+    <div className="body">
+      {article === null ? <LoadingSpinner /> : article.map((item) => <News article={item} />)}
+    </div>
+  );
+};
+
+export default Body;
