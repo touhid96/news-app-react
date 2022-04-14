4f5bdb9e53984c77e864d896d5f0d17f50f9b50b
Touhidul_Islam <touhid.islam96@gmail.com>
2022-04-15 04:42:28 +0600

    api key changed

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "../../App.css";
 import LoadingSpinner from "./LoadingSpinner";
 import News from "./News/News";
@@ -6,12 +6,12 @@ import News from "./News/News";
 const Body = () => {
   const [article, setArticle] = useState([]);
 
-  useEffect(() => {
-    fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=5e005fc4211a49ac956db9183c8466ef")
-      .then((res) => res.json())
-      .then((data) => setArticle(data.articles))
-      .catch((error) => console.error(error));
-  }, []);
+  // useEffect(() => {
+  //   fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=5e005fc4211a49ac956db9183c8466ef")
+  //     .then((res) => res.json())
+  //     .then((data) => setArticle(data.articles))
+  //     .catch((error) => console.error(error));
+  // }, []);
   console.log(article);
 
   return (
